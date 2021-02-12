3434d996c32d0691a92e20ff64de12e4f3895f85
Maya Alli <56855612+mayaalli78@users.noreply.github.com>
2021-02-12 14:43:59 -0600

    Update script.js

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,7 +1,26 @@
-const images = document.querySelectorAll('.imageClass');
-// eslint-disable-next-line no-unused-vars
-const imageArray = Array.from(images);
-array1.forEach((element) => {
-  // eslint-disable-next-line no-console
-  console.log(element);
-});
+const { event } = require("cypress/types/jquery");
+
+function arrayMethodDemo() {
+  const array1 =['onigirl_1.png', 'onigirl_2.png', 'onigirl_3.png', 'onigirl_4.png', 'roll_1.png', 'roll_2.png', 'roll_3.png' ]; 
+  const listContainer = document.createElement('ul');
+  const target = document.querySelector('#box1');
+  target.append(listContainer);
+
+  const array2 = array1.map(element => {
+    const listItem = document.createElement('li');
+    listItem.InnerText = element;
+    listContainer.append(listItem);
+    return typeof element;
+  })
+
+  console.log(array2);
+}
+
+function onLoadOfPage() {
+    document.addEventListener('click', (event) => {
+    arrayMethodDemo()
+    });
+}
+
+window.onload = onLoadOfPage
+
