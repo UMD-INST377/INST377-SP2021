415b937045590827c179f9c8349290bb29fa7e94
Joseph Sanchez <sanchezjoseph378@gmail.com>
2021-02-16 19:18:55 -0500

    EDIT: Quotes

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,20 +1,20 @@
 let width = 130;
 let count = 3;
-let list = carousel.querySelector("ul");
-let listPosition = carousel.querySelectorAll("li");
+let list = carousel.querySelector('ul');
+let listPosition = carousel.querySelectorAll('li');
 let position = 0;
-const leftButton = document.querySelector("button.arrow.prev");
-const rightButton = document.querySelector("button.arrow.next");
+const leftButton = document.querySelector('button.arrow.prev');
+const rightButton = document.querySelector('button.arrow.next');
 
 function leftOfImage() {
-  leftButton.addEventListener("click", (event) => {
+  leftButton.addEventListener('click', (event) => {
     position += width * count;
     position = Math.min(position, 0);
   });
 }
 
 function rightOfImage() {
-  rightButton.addEventListener("click", (event) => {
+  rightButton.addEventListener('click', (event) => {
     position -= width * count;
     position = Math.max(position, -width * (listPosition.length - count));
   });
