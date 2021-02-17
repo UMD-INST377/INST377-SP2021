8a15ebf1bfb85fbb4c7db0c6aa233f8a9492db94
Joseph Sanchez <sanchezjoseph378@gmail.com>
2021-02-16 19:16:59 -0500

    ADD: JS Positioning and clickability

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,9 +1,24 @@
-/*
-*function changePhoto() {
-  const photos = document.querySelectorAll('.images');
-  const photoArray = Array.from(photos);
+let width = 130;
+let count = 3;
+let list = carousel.querySelector("ul");
+let listPosition = carousel.querySelectorAll("li");
+let position = 0;
+const leftButton = document.querySelector("button.arrow.prev");
+const rightButton = document.querySelector("button.arrow.next");
 
+function leftOfImage() {
+  leftButton.addEventListener("click", (event) => {
+    position += width * count;
+    position = Math.min(position, 0);
+  });
 }
-changePhoto();
 
-*/
+function rightOfImage() {
+  rightButton.addEventListener("click", (event) => {
+    position -= width * count;
+    position = Math.max(position, -width * (listPosition.length - count));
+  });
+}
+
+window.onload = leftOfImage;
+window.onload = rightOfImage;
