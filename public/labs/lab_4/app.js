d0df46349d1d94bea0340e424b5856bcfc474403
JaredAveryCaplan <78220609+JaredAveryCaplan@users.noreply.github.com>
2021-02-19 16:34:31 -0500

    js

--- public/labs/lab_4/app.js
+++ public/labs/lab_4/app.js
@@ -1,25 +1,3 @@
-const express = require("express");
-const bodyParser = require("body-parser")
- 
-
-const app = express();
-app.use(bodyParser.urlencoded({
-    extended:true
-}));
- 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
- 
-app.post("/", function(req, res) {
-  var num1 = Number(req.body.num1);
-  var num2 = Number(req.body.num2);
-   
-  var result = num1 + num2 ;
-   
-  res.send("Addition - " + result);
-});
- 
-app.listen(3000, function(){
-  console.log("server is running on port 3000");
-})
+fetch('http://localhost:3000/labs/lab_4/?', {method: 'POST', body: 'JC Lab 4'})
+    .then(results => results.json())
+    .then(console.log);
