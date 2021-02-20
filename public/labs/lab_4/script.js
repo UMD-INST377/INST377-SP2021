10f96cca4e72649cb74c7b21b49d87584761bb7c
Maya Alli <56855612+mayaalli78@users.noreply.github.com>
2021-02-19 22:45:25 -0600

    changes

--- public/labs/lab_4/script.js
+++ public/labs/lab_4/script.js
@@ -1,6 +1,15 @@
 const { on } = require('nodemon');
 
 function onLoad() {
-  res.json('hello world')
+  console.log('script loaded');
 }
 window.onLoad = onLoad;
+
+app.route('/api')
+.post(async (req, res) => {
+  console.log('POST request detected');
+  console.log('Form data in res.body', req.body);
+  res.json('hello world');
+
+} 
+
--- server.js
+++ server.js
@@ -42,7 +42,6 @@ app.route('/api')
   .post(async (req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
-    //console.log('Now send something back to your client');
     res.json('hello world');
     // res.json({data: dataToSendToFrontEnd});
   });
