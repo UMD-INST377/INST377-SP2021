584475d3b2d91a2fdf28ee163eaa31b974101486
Maya Alli <56855612+mayaalli78@users.noreply.github.com>
2021-02-23 16:01:32 -0600

    Update script.js

--- public/labs/lab_4/script.js
+++ public/labs/lab_4/script.js
@@ -6,10 +6,8 @@ function onLoad() {
 window.onLoad = onLoad;
 
 app.route('/api')
-.post(async (req, res) => {
-  console.log('POST request detected');
-  console.log('Form data in res.body', req.body);
-  res.json('hello world');
-
-} 
-
+  .post(async (req, res) => {
+    console.log('POST request detected');
+    console.log('Form data in res.body', req.body);
+    res.json('hello world');
+  });
