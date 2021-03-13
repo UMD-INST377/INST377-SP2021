ae8dab1289a77c4cad6f9655dfdd798bff80dc11
Joseph Sanchez <sanchezjoseph378@gmail.com>
2021-03-12 19:34:34 -0500

    Map addition

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,7 +1,17 @@
 /* eslint-disable linebreak-style */
 function mapInit() {
   // follow the Leaflet Getting Started tutorial here
-  return map;
+  // latitude then longitude
+  const mymap = L.map('mapid').setView([38.9897, -76.9378], 13);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoiam9lam9lam9lY2F0IiwiYSI6ImNrbTJibzFyZjBmN2UycGp2cDQ5bnN6dGIifQ.tOfDHoWiiSyZJCY3lAhMew'
+  }).addTo(mymap);
+  return mymap;
 }
 
 async function dataHandler(mapObjectFromFunction) {
