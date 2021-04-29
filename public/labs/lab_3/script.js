3705aca34b71ccc76b510c2c1ea9a16f3cc1d93a
JaredAveryCaplan <78220609+JaredAveryCaplan@users.noreply.github.com>
2021-04-29 16:33:57 -0400

    j

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,10 +1,7 @@
-var images =new Array();
+function clickToMove() {
+    co
+    
+    document.addEventListener('click',(event) =>){
 
-images[1] = 'images/onigiri_1.png' 
-images[2] = 'images/onigiri_2.png'
-images[3] = 'images/onigiri_4.png'
-images[4] = 'images/roll_1.png'
-images[5] = 'images/roll_2.png'
-images[6] = 'images/roll_3.png'
-
-document.write(images);
+    }
+}
